0e744ef4e85df7f5a03eb643a29f1bf314e752fe
Lautaro Aimar <lautaroaimarb@gmail.com>
2017-04-21 17:38:00 -0300

    JavaDoc Alianza
    Se documentó la clase, método constructor y eliminarPersonaje.

--- src/main/java/dominio/Alianza.java
+++ src/main/java/dominio/Alianza.java
@@ -1,12 +1,22 @@
 package dominio;
 
 import java.util.LinkedList;
-
+/**
+ * Clase encargada de agrupar con un nombre específico de alianza a los personajes que están 
+ * aliados.
+ *
+ */
 public class Alianza {
 
 	String nombre;
 	LinkedList<Personaje> aliados;
-
+/**
+ * <h3><u>Constructor de una Alianza</u></h3>
+ * <p>Se recibirá un nombre (String) para definir la Alianza y 
+ * se asociará el miembro "aliados" a un nuevo LinkedList de personajes.<br> 
+ * Es importante saber dicho nombre no tiene la posibilidad de ser modificado luego de creada la Alianza.</p>
+ * @param nombre Es un String que servirá de nombre de la Alianza a crear.
+ */
 	public Alianza(String nombre) {
 		this.nombre = nombre;
 		this.aliados = new LinkedList <Personaje>();
@@ -23,7 +33,11 @@ public class Alianza {
 	public String obtenerNombre(){
 		return nombre;
 	}
-	
+	/**
+	 * <h3><u>Método eliminarPersonaje</u></h3>
+	 * <p>Método encargado de la eliminación de un personaje de la alianza llamadora del método.</p>
+	 * @param pj Objeto (de tipo Personaje) a eliminar del LinkedList compuesto por personajes aliados entre sí.
+	 */
 	public void eliminarPersonaje(Personaje pj){
 		aliados.remove(pj);
 	}
