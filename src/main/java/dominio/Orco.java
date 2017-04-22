96d9d3a483bcc32ffa4056f1fdf17a1142aa6421
Lautaro Aimar <lautaroaimarb@gmail.com>
2017-04-22 01:45:35 -0300

    JavaDoc Orco.java
    Se documenta Golpe Defensa, Mordisco de Vida y la clase.

--- src/main/java/dominio/Orco.java
+++ src/main/java/dominio/Orco.java
@@ -1,5 +1,11 @@
 package dominio;
-
+/**
+ * 
+ * Clase que define un Personaje del tipo "Orco", por lo tanto extiende 
+ * a la clase abstracta Personaje.
+ * (NO tiene variables miebro propias)
+ *
+ */
 public class Orco extends Personaje {
 
 	public Orco(String nombre, Casta casta, int id) {
@@ -25,6 +31,16 @@ public class Orco extends Personaje {
 	}
 
 	// Golpe Defensa
+	/**
+	 * <h3><u>Golpe Defensa</u></h3>
+	 * Intenta un golpe usando el valor de su defensa 
+	 * (provoca un daño de hasta un max igual al doble del valor de la defensa del Orco).<p>
+	 * En caso de poder realizarse se reduce la energia en 10 y se devuelve true.<br>
+	 * Caso contrario devuelve false y no se pierde energia.
+	 * 
+	 * @param atacado Peleable a ser atacado por el Orco.
+	 * @return true en caso de realizarse el ataque, false en caso contrario.
+	 */
 	public boolean habilidadRaza1(Peleable atacado) {
 		if (this.getEnergia() > 10) {
 			this.setEnergia(this.getEnergia() - 10);
@@ -35,6 +51,14 @@ public class Orco extends Personaje {
 	}
 
 	// Mordisco de Vida
+	/**
+	 * <h3><u>Mordisco de Vida</u></h3>
+	 * Siempre que la energia del Orco lo permita.<br>
+	 * Si luego de un ataque de un Orco a un objeto Peleable, el daño causado es mayor que cero; 
+	 * dicho objeto recupera vida por el mismo valor de daño que hubiese causado el Orco.
+	 * @param atacado Peleable mordido por el Orco.
+	 * @return true en caso de realizarse la mordida, false en caso contrario.
+	 */
 	public boolean habilidadRaza2(Peleable atacado) {
 		if (this.getEnergia() > 10) {
 			this.setEnergia(this.getEnergia() - 10);
