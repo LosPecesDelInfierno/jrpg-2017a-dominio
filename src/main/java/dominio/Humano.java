bbbe07d42e0951fca7f4d08413ed2d660be48874
Lautaro Aimar <lautaroaimarb@gmail.com>
2017-04-21 23:09:45 -0300

    Javadoc Humano.java
    Se documenta clase, constructor por defecto, y el método habilidadRaza2 (abstracto de Personaje.java)

--- src/main/java/dominio/Humano.java
+++ src/main/java/dominio/Humano.java
@@ -1,7 +1,21 @@
 package dominio;
-
+/**
+ * 
+ * Clase que define un Personaje del tipo "Humano", por lo tanto extiende 
+ * a la clase abstracta Personaje.
+ * (NO tiene variables miebro propias)
+ *
+ */
 public class Humano extends Personaje {
-
+/**
+ * <h3><u>Método Constructor por defecto</u></h3>
+ * Recibe los parámetros específicos del constructor parametrizado de su clase padre (nombre, casta , id),
+ * llama al constructor parametrizado padre con dichos parametros 
+ * y define por defecto saludTope, energiaTope, salud, energia y nombreRaza
+ * @param nombre Es un String que representa el nombre del Humano a crear.
+ * @param casta Variable del tipo Casta que determina a que casta pertenecerá.
+ * @param id Es un int que representa el id del personaje.
+ */
 	public Humano(String nombre, Casta casta, int id) {
 		super(nombre, casta, id);
 		saludTope += 5;
@@ -32,6 +46,12 @@ public class Humano extends Personaje {
 	}
 
 	// Golpe Fatal
+	/**
+	 * <h3><u>Golpe Fatal</u></h3>
+	 * Intenta un golpe fatal (provoca hasta un max de la mitad de la vida del objeto Peleable atacado).<p>
+	 * En caso de poder realizarse, se reduce la energía a la mitad retornando un boolean true.<br>
+	 * Caso contrario resta energía al objeto llamador y devuelve false.
+	 */
 	public boolean habilidadRaza2(Peleable atacado) {
 		if (this.getEnergia() > 10) {
 			if (atacado.serAtacado(atacado.getSalud() / 2) > 0) {
