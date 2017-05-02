d797a13469cd5c8fbbf7fa88a82c1e5ec7fb98cb
Unknown <lautaroaimarb@gmail.com>
2017-05-02 18:32:35 -0300

    Se lleva Coverage a 90.4%
    Se sigue usando eclEmma..

--- src/test/java/tests_dominio/TestNPC.java
+++ src/test/java/tests_dominio/TestNPC.java
@@ -1,15 +1,84 @@
 package tests_dominio;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import dominio.Hechicero;
+import dominio.Humano;
 import dominio.NonPlayableCharacter;
 
 public class TestNPC {
-
+	NonPlayableCharacter npc;
+	
+	@Before
+	public void setUpBeforeTests(){
+		NonPlayableCharacter npc = new NonPlayableCharacter("Gigante", 1, -1);
+	}
 	@Test
 	public void testOtorgarExp() {
 		NonPlayableCharacter npc = new NonPlayableCharacter("Gigante", 1, -1);
-		Assert.assertTrue(30 == npc.otorgarExp());
+		Assert.assertTrue(Integer.valueOf(30).equals(Integer.valueOf(npc.otorgarExp())));
+		//Assert.assertTrue(30 == npc.otorgarExp());
+	}
+	@Test
+	public void testAtacar(){
+		Humano humano = new Humano("Lautaro", new Hechicero(), 1);
+		NonPlayableCharacter npc = new NonPlayableCharacter("Gigante", 1, -1);
+		Assert.assertNotEquals(0, (npc.atacar(humano))); //20 de danio
+	}
+	@Test
+	public void testSerAtacado(){
+		Humano humano = new Humano("Lautaro", new Hechicero(), 1);
+		NonPlayableCharacter npc = new NonPlayableCharacter("Gigante", 1, -1);
+		Assert.assertNotEquals(0, (humano.atacar(npc))); //13 de danio
+	}
+	@Test
+	public void testDespuesDeTurno(){
+		NonPlayableCharacter npc = new NonPlayableCharacter("Gigante", 1, -1);
+		npc.despuesDeTurno();
+		Assert.assertNotNull(npc);
+	}
+	@Test
+	public void testGanarExperiencia(){
+		NonPlayableCharacter npc = new NonPlayableCharacter("Gigante", 1, -1);
+		npc.ganarExperiencia((int)Math.random());
+		Assert.assertNotNull(npc);
+	}
+	@Test
+	public void testsetYGetAtaque(){
+		NonPlayableCharacter npc = new NonPlayableCharacter("Gigante", 1, -1);
+		npc.setAtaque(20);
+		Assert.assertEquals(20, npc.getAtaque());
+	}
+	@Test
+	public void testsetYGetFuerza(){
+		NonPlayableCharacter npc = new NonPlayableCharacter("Gigante", 1, -1);
+		npc.setFuerza(20);
+		Assert.assertEquals(20, npc.getFuerza());
+	}
+	@Test
+	public void testsetYGetNivel(){
+		NonPlayableCharacter npc = new NonPlayableCharacter("Gigante", 1, -1);
+		npc.setNivel(20);
+		Assert.assertEquals(20, npc.getNivel());
+	}
+	@Test
+	public void testsetYGetNombre(){
+		NonPlayableCharacter npc = new NonPlayableCharacter("Gigante", 1, -1);
+		npc.setNombre("Lautaro");
+		Assert.assertEquals("Lautaro", npc.getNombre());
+	}
+	@Test
+	public void testsetYGetSalud(){
+		NonPlayableCharacter npc = new NonPlayableCharacter("Gigante", 1, 1); //Pruebo caso nivel 1
+		npc.setSalud(5);
+		Assert.assertEquals(5, npc.getSalud());
+	}
+	@Test
+	public void testsetYGetDefensa(){
+		NonPlayableCharacter npc = new NonPlayableCharacter("Gigante", 1, -1);
+		npc.setDefensa(7);
+		Assert.assertEquals(7, npc.getDefensa());
 	}
 }
--- src/test/java/tests_dominio/TestPersonaje.java
+++ src/test/java/tests_dominio/TestPersonaje.java
@@ -10,72 +10,78 @@ public class TestPersonaje {
 	@Test
 	public void testHumano() {
 		Humano h = new Humano("Nicolas", new Guerrero(), 1);
-		Assert.assertTrue(h.getSalud() == 105);
-		Assert.assertTrue(h.getEnergia() == 105);
-		Assert.assertTrue(h.getFuerza() == 15);
-		Assert.assertTrue(h.getDestreza() == 10);
-		Assert.assertTrue(h.getInteligencia() == 10);
+		Assert.assertTrue(Integer.valueOf(h.getSalud()).equals(Integer.valueOf(105)));
+		Assert.assertTrue(Integer.valueOf(h.getEnergia()).equals(Integer.valueOf(105)));
+		Assert.assertTrue(Integer.valueOf(h.getFuerza()).equals(Integer.valueOf(15)));
+		Assert.assertTrue(Integer.valueOf(h.getDestreza()).equals(Integer.valueOf(10)));
+		Assert.assertTrue(Integer.valueOf(h.getInteligencia()).equals(Integer.valueOf(10)));
 
 		Humano h2 = new Humano("Lautaro", new Hechicero(), 2);
-		Assert.assertTrue(h2.getSalud() == 105);
-		Assert.assertTrue(h2.getEnergia() == 105);
-		Assert.assertTrue(h2.getFuerza() == 10);
-		Assert.assertTrue(h2.getDestreza() == 10);
-		Assert.assertTrue(h2.getInteligencia() == 15);
+		Assert.assertTrue(Integer.valueOf(h2.getSalud()).equals(Integer.valueOf(105)));
+		Assert.assertTrue(Integer.valueOf(h2.getEnergia()).equals(Integer.valueOf(105)));
+		Assert.assertTrue(Integer.valueOf(h2.getFuerza()).equals(Integer.valueOf(10)));
+		Assert.assertTrue(Integer.valueOf(h2.getDestreza()).equals(Integer.valueOf(10)));
+		Assert.assertTrue(Integer.valueOf(h2.getInteligencia()).equals(Integer.valueOf(15)));
 
 		Humano h3 = new Humano("Hernan", new Asesino(), 3);
-		Assert.assertTrue(h3.getSalud() == 105);
-		Assert.assertTrue(h3.getEnergia() == 105);
-		Assert.assertTrue(h3.getFuerza() == 10);
-		Assert.assertTrue(h3.getDestreza() == 15);
-		Assert.assertTrue(h3.getInteligencia() == 10);
+		int aux = h3.getSalud();
+		Assert.assertTrue(Integer.valueOf(aux).equals(Integer.valueOf(105)));
+		aux = h3.getEnergia();
+		Assert.assertTrue(Integer.valueOf(aux).equals(Integer.valueOf(105)));
+		Assert.assertTrue(Integer.valueOf(h3.getFuerza()).equals(Integer.valueOf(10)));	
+		Assert.assertTrue(Integer.valueOf(h3.getDestreza()).equals(Integer.valueOf(15)));
+		Assert.assertTrue(Integer.valueOf(h3.getInteligencia()).equals(Integer.valueOf(10)));
 	}
 
 	@Test
 	public void testElfo() {
 		Elfo e = new Elfo("Nicolas", new Guerrero(), 1);
-		Assert.assertTrue(e.getSalud() == 100);
-		Assert.assertTrue(e.getEnergia() == 110);
-		Assert.assertTrue(e.getFuerza() == 15);
-		Assert.assertTrue(e.getDestreza() == 10);
-		Assert.assertTrue(e.getInteligencia() == 10);
+		Assert.assertTrue(Integer.valueOf(e.getSalud()).equals(Integer.valueOf(100)));
+		Assert.assertTrue(Integer.valueOf(e.getEnergia()).equals(Integer.valueOf(110)));
+		Assert.assertTrue(Integer.valueOf(e.getFuerza()).equals(Integer.valueOf(15)));
+		Assert.assertTrue(Integer.valueOf(e.getDestreza()).equals(Integer.valueOf(10)));
+		Assert.assertTrue(Integer.valueOf(e.getInteligencia()).equals(Integer.valueOf(10)));
 
 		Elfo e2 = new Elfo("Lautaro", new Hechicero(), 2);
-		Assert.assertTrue(e2.getSalud() == 100);
-		Assert.assertTrue(e2.getEnergia() == 110);
-		Assert.assertTrue(e2.getFuerza() == 10);
-		Assert.assertTrue(e2.getDestreza() == 10);
-		Assert.assertTrue(e2.getInteligencia() == 15);
+		Assert.assertTrue(Integer.valueOf(e2.getSalud()).equals(Integer.valueOf(100)));
+		Assert.assertTrue(Integer.valueOf(e2.getEnergia()).equals(Integer.valueOf(110)));
+		Assert.assertTrue(Integer.valueOf(e2.getFuerza()).equals(Integer.valueOf(10)));
+		Assert.assertTrue(Integer.valueOf(e2.getDestreza()).equals(Integer.valueOf(10)));
+		Assert.assertTrue(Integer.valueOf(e2.getInteligencia()).equals(Integer.valueOf(15)));
 
 		Elfo e3 = new Elfo("Hernan", new Asesino(), 3);
-		Assert.assertTrue(e3.getSalud() == 100);
-		Assert.assertTrue(e3.getEnergia() == 110);
-		Assert.assertTrue(e3.getFuerza() == 10);
-		Assert.assertTrue(e3.getDestreza() == 15);
-		Assert.assertTrue(e3.getInteligencia() == 10);
+		int aux = e3.getSalud();
+		Assert.assertTrue(Integer.valueOf(aux).equals(Integer.valueOf(100)));
+		aux = e3.getEnergia();
+		Assert.assertTrue(Integer.valueOf(aux).equals(Integer.valueOf(110)));
+		Assert.assertTrue(Integer.valueOf(e3.getFuerza()).equals(Integer.valueOf(10)));	
+		Assert.assertTrue(Integer.valueOf(e3.getDestreza()).equals(Integer.valueOf(15)));
+		Assert.assertTrue(Integer.valueOf(e3.getInteligencia()).equals(Integer.valueOf(10)));
 	}
 
 	@Test
 	public void testOrco() {
 		Orco o = new Orco("Nicolas", new Guerrero(), 1);
-		Assert.assertTrue(o.getSalud() == 110);
-		Assert.assertTrue(o.getEnergia() == 100);
-		Assert.assertTrue(o.getFuerza() == 15);
-		Assert.assertTrue(o.getDestreza() == 10);
-		Assert.assertTrue(o.getInteligencia() == 10);
+		Assert.assertTrue(Integer.valueOf(o.getSalud()).equals(Integer.valueOf(110)));
+		Assert.assertTrue(Integer.valueOf(o.getEnergia()).equals(Integer.valueOf(100)));
+		Assert.assertTrue(Integer.valueOf(o.getFuerza()).equals(Integer.valueOf(15)));
+		Assert.assertTrue(Integer.valueOf(o.getDestreza()).equals(Integer.valueOf(10)));
+		Assert.assertTrue(Integer.valueOf(o.getInteligencia()).equals(Integer.valueOf(10)));
 
 		Orco o2 = new Orco("Lautaro", new Hechicero(), 2);
-		Assert.assertTrue(o2.getSalud() == 110);
-		Assert.assertTrue(o2.getEnergia() == 100);
-		Assert.assertTrue(o2.getFuerza() == 10);
-		Assert.assertTrue(o2.getDestreza() == 10);
-		Assert.assertTrue(o2.getInteligencia() == 15);
+		Assert.assertTrue(Integer.valueOf(o2.getSalud()).equals(Integer.valueOf(110)));
+		Assert.assertTrue(Integer.valueOf(o2.getEnergia()).equals(Integer.valueOf(100)));
+		Assert.assertTrue(Integer.valueOf(o2.getFuerza()).equals(Integer.valueOf(10)));
+		Assert.assertTrue(Integer.valueOf(o2.getDestreza()).equals(Integer.valueOf(10)));
+		Assert.assertTrue(Integer.valueOf(o2.getInteligencia()).equals(Integer.valueOf(15)));
 
 		Orco o3 = new Orco("Hernan", new Asesino(), 3);
-		Assert.assertTrue(o3.getSalud() == 110);
-		Assert.assertTrue(o3.getEnergia() == 100);
-		Assert.assertTrue(o3.getFuerza() == 10);
-		Assert.assertTrue(o3.getDestreza() == 15);
-		Assert.assertTrue(o3.getInteligencia() == 10);
+		int aux = o3.getSalud();
+		Assert.assertTrue(Integer.valueOf(aux).equals(Integer.valueOf(110)));
+		aux = o3.getEnergia();
+		Assert.assertTrue(Integer.valueOf(aux).equals(Integer.valueOf(100)));
+		Assert.assertTrue(Integer.valueOf(o3.getFuerza()).equals(Integer.valueOf(10)));	
+		Assert.assertTrue(Integer.valueOf(o3.getDestreza()).equals(Integer.valueOf(15)));
+		Assert.assertTrue(Integer.valueOf(o3.getInteligencia()).equals(Integer.valueOf(10)));
 	}
 }
