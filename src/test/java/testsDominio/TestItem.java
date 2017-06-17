1d3cdbf5516c7a2366dcca80c71430d6d9dcdb89
Lautaro <lautaroaimarb@gmail.com>
2017-06-17 15:41:25 -0300

    TestItem en verde!

--- src/test/java/testsDominio/TestItem.java
+++ src/test/java/testsDominio/TestItem.java
@@ -1,5 +1,6 @@
 package testsDominio;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,35 +9,40 @@ import dominio.AtributoModificable;
 import dominio.Humano;
 import dominio.Item;
 import dominio.ModificadorItem;
-import junit.framework.Assert;
+import mensajeria.PaquetePersonaje;
 
-@SuppressWarnings("deprecation")
+
+
 public class TestItem {
 
 	private Humano h;
 	private Item item;
+	private PaquetePersonaje pp;
 	@Before
 	public void setear() {
 		item = new Item(1, "Espada", 4, 10, 10, 10, "Foto");
 		h = new Humano("Nico", 100, 100, 25, 20, 30, new Asesino(0.2, 0.3, 1.5), 0, 1, 1);
+		pp = new PaquetePersonaje();
 	}
 	
 	@Test
 	public void addItemInventario() {
 		ModificadorItem fuerza = new ModificadorItem(AtributoModificable.FUERZA, 10, false);
 		item.addModificador(fuerza);
-		Assert.assertTrue(Integer.valueOf(25).equals(Integer.valueOf(h.getFuerza())));
-//		h.addItemInventario(item);
-		Assert.assertTrue(Integer.valueOf(35).equals(Integer.valueOf(h.getFuerza())));
+		pp.setFuerza(25);
+		Assert.assertTrue(Integer.valueOf(pp.getFuerza()).equals(Integer.valueOf(25)));
+		pp.agregarItem(item);
+		Assert.assertTrue(Integer.valueOf(pp.getFuerza()).equals(Integer.valueOf(35)));
 	}
 	
 	@Test
 	public void ModificadorItemConPorcentaje() {
 		ModificadorItem inteligencia = new ModificadorItem(AtributoModificable.INTELIGENCIA, 20, true);
 		item.addModificador(inteligencia);
-		Assert.assertTrue(Integer.valueOf(30).equals(Integer.valueOf(h.getInteligencia())));
-//		h.addItemInventario(item);
-		Assert.assertTrue(Integer.valueOf(36).equals(Integer.valueOf(h.getInteligencia())));
+		pp.setInteligencia(25);
+		Assert.assertTrue(Integer.valueOf(25).equals(Integer.valueOf(pp.getInteligencia())));
+		pp.agregarItem(item);
+		Assert.assertTrue(Integer.valueOf(30).equals(Integer.valueOf(pp.getInteligencia())));
 	}
 	
 	
@@ -50,15 +56,24 @@ public class TestItem {
 		item.addModificador(salud);
 		item.addModificador(energia);
 		item.addModificador(fuerza);
-		Assert.assertTrue(Integer.valueOf(30).equals(Integer.valueOf(h.getInteligencia())));
-		Assert.assertTrue(Integer.valueOf(100).equals(Integer.valueOf(h.getSalud())));
-		Assert.assertTrue(Integer.valueOf(100).equals(Integer.valueOf(h.getEnergia())));
-		Assert.assertTrue(Integer.valueOf(25).equals(Integer.valueOf(h.getFuerza())));
-//		h.addItemInventario(item);
-		Assert.assertTrue(Integer.valueOf(34).equals(Integer.valueOf(h.getInteligencia())));
-		Assert.assertTrue(Integer.valueOf(110).equals(Integer.valueOf(h.getSalud())));
-		Assert.assertTrue(Integer.valueOf(105).equals(Integer.valueOf(h.getEnergia())));
-		Assert.assertTrue(Integer.valueOf(27).equals(Integer.valueOf(h.getFuerza())));
+		pp.setSaludTope(100); //Salud base pasa a ser 100
+		pp.setInteligencia(60);
+		pp.setEnergiaTope(75); //Energia base pasa a ser 75
+		pp.setFuerza(30);
+		Assert.assertTrue(Integer.valueOf(60).equals(Integer.valueOf(pp.getInteligencia())));
+		Assert.assertTrue(Integer.valueOf(100).equals(Integer.valueOf(pp.getSaludTope())));
+		Assert.assertTrue(Integer.valueOf(75).equals(Integer.valueOf(pp.getEnergiaTope())));
+		Assert.assertTrue(Integer.valueOf(30).equals(Integer.valueOf(pp.getFuerza())));
+		pp.agregarItem(item);
+		Assert.assertNotEquals(pp.getInteligenciaBase(), pp.getInteligencia());
+		Assert.assertNotEquals(pp.getSaludBase(), pp.getSaludTope());
+		Assert.assertNotEquals(pp.getFuerzaBase(), pp.getFuerza());
+		Assert.assertNotEquals(pp.getEnergiaBase(), pp.getEnergiaTope());
+		Assert.assertTrue(Integer.valueOf(69).equals(Integer.valueOf(pp.getInteligencia())));
+		Assert.assertTrue(Integer.valueOf(110).equals(Integer.valueOf(pp.getSaludTope())));
+		Assert.assertTrue(Integer.valueOf(80).equals(Integer.valueOf(pp.getEnergiaTope())));
+		Assert.assertTrue(Integer.valueOf(33).equals(Integer.valueOf(pp.getFuerza())));
+		
 	}
 	
 	@Test
@@ -66,21 +81,23 @@ public class TestItem {
 		Item item2 = new Item(2, "Botas", 6, 10, 10, 10, "Foto");
 		Item item3 = new Item(3, "Escudo", 5, 10, 10, 10, "Foto");
 		Item item4 = new Item(4, "Armadura", 2, 10, 10, 10, "Foto");
-		ModificadorItem salud1 = new ModificadorItem(AtributoModificable.SALUD, 15, true);
-		ModificadorItem salud2 = new ModificadorItem(AtributoModificable.SALUD, 10, false);
+		ModificadorItem salud1 = new ModificadorItem(AtributoModificable.SALUD, 5, false); 
+		ModificadorItem salud2 = new ModificadorItem(AtributoModificable.SALUD, 10, false); 
 		ModificadorItem salud3 = new ModificadorItem(AtributoModificable.SALUD, 5, false);
-		ModificadorItem salud4 = new ModificadorItem(AtributoModificable.SALUD, 10, true);
+		ModificadorItem salud4 = new ModificadorItem(AtributoModificable.SALUD, 10, false); 
 		item.addModificador(salud1);
 		item2.addModificador(salud2);
 		item3.addModificador(salud3);
 		item4.addModificador(salud4);
-		Assert.assertTrue(Integer.valueOf(100).equals(Integer.valueOf(h.getSalud())));
-//		h.addItemInventario(item);
-//		h.addItemInventario(item2);
-//		h.addItemInventario(item3);
-//		h.addItemInventario(item4);
-		System.out.println(h.getSalud());
-		Assert.assertTrue(Integer.valueOf(141).equals(Integer.valueOf(h.getSalud())));
+		pp.setSaludTope(180); // Salud Base es 180
+		//System.out.println(pp.getSaludBase());
+		Assert.assertTrue(Integer.valueOf(180).equals(Integer.valueOf(pp.getSaludTope())));
+		pp.agregarItem(item);
+		pp.agregarItem(item2);
+		pp.agregarItem(item3);
+		pp.agregarItem(item4);
+		Assert.assertNotEquals(pp.getSaludBase(), pp.getSaludTope());
+		Assert.assertTrue(Integer.valueOf(210).equals(Integer.valueOf(pp.getSaludTope())));
 	}
 	
 	@Test
