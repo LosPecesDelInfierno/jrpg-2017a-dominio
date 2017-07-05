fdc87375846fa0cfca8f6a943be62716b2b246dd
Unknown <marianoegam@gmail.com>
2017-07-05 00:23:04 -0300

    Mas logica de intercambio

--- src/main/java/mensajeria/PaqueteIntercambio.java
+++ src/main/java/mensajeria/PaqueteIntercambio.java
@@ -1,16 +1,34 @@
 package mensajeria;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class PaqueteIntercambio extends Paquete implements Serializable, Cloneable {
 
+	private String nombre;
 	private int id;
 	private int idEnemigo;
 	private boolean[] objetosPersonaje = { false, false, false, false, false, false, false, false };
 	private boolean[] objetosEnemigo = { false, false, false, false, false, false, false, false };
+	private List<String> listaPersonaje;
+	private List<String> listaEnemigo;
 
 	public PaqueteIntercambio() {
 		setComando(Comando.INTERCAMBIAR);
+		listaPersonaje = new ArrayList<>();
+		listaEnemigo = new ArrayList<>();
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
 	public int getId() {
@@ -43,5 +61,25 @@ public class PaqueteIntercambio extends Paquete implements Serializable, Cloneab
 
 	public boolean getSeleccionadoEnemigo(int idx) {
 		return objetosEnemigo[idx];
+	}
+	
+	public void addDescripcionPersonaje(String desc) {
+		listaPersonaje.add(desc);
+	}
+	
+	public void addDescripcionEnemigo(String desc) {
+		listaEnemigo.add(desc);
+	}
+	
+	public List<String> getListaPersonaje() {
+		List<String> aux = new ArrayList<String>();
+		Collections.copy(aux, listaPersonaje);
+		return aux;
+	}
+	
+	public List<String> getListaEnemigo() {
+		List<String> aux = new ArrayList<String>();
+		Collections.copy(aux, listaEnemigo);
+		return aux;
 	}
 }
